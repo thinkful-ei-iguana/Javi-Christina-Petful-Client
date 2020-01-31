29bad3afe31fef0514aa9b40df5eb595c00457b2
Christina Chapman <cmchapman75@gmail.com>
2020-01-31 14:51:53 -0600

    Client Side Beginning

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -6,7 +6,8 @@ class Landing extends React.Component {
     return (
       <div className="home">
         <h1>Welcome to Petful Animal Shelter</h1>
-        <p>We are a shelter for dogs and cats that need a loving home.  Once started, we will show you the next cat and dog that need to be adopted.  Animals will be adopted on a first in, first out basis.  If you wish, you may wait in line for the new set of animals to be adopted.</p>
+        <p>We are a shelter for dogs and cats that need a loving home.  Once started, 
+          we will show you the next cat and dog that need to be adopted.  Animals will be adopted on a first in, first out basis.  If you wish, you may wait in line for the new set of animals to be adopted.</p>
         <p>Our animals are eager to meet you!</p>
         <img className="cat-dog-img" alt="cute cat and dog" src="https://i.pinimg.com/originals/bf/c9/69/bfc969fb4f7d4faa7d81583de32573b5.jpg" ></img>
         <br></br>
