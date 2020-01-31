6ea6b6bd1ad931e325c194078bb9d133f5bcc775
Christina Chapman <cmchapman75@gmail.com>
2020-01-31 15:19:54 -0600

    added deployed server api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://shrouded-anchorage-72118.herokuapp.com/api`
+    API_ENDPOINT: `https://peaceful-plains-00984.herokuapp.com/api`
     // API_ENDPOINT: 'http://localhost:8000/api'
   }
